6facdbcbd7943405ae8461e5fd11e9356b33ca65
Deepak Prasad Gupta <deepak2006gupta@outlook.com>
2026-05-26 21:58:57 +0530

    util code creation

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (fn) => async(req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+        success: false,
+        message: error.message
+    });
+  }
+};
+
+export { asyncHandler };
+
+
+// Alternative implementation using Promise.resolve
+
+/*
+
+const asyncHandler = (asyncFn) =>{
+    (req, res, next) => {
+        Promise.resolve(asyncFn(req, res, next)).catch(err => next(err));
+    }
+}
+
+*/
