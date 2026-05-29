772b5fabd1b71c7ae90c3fe3fb278b04345345de
Deepak Prasad Gupta <deepak2006gupta@outlook.com>
2026-05-29 22:13:20 +0530

    installed bcrypt and jwt and used bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
     unsername:{
@@ -44,5 +46,16 @@ const userSchema = new Schema({
     },
 
 }, {timestamps: true});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isValidPassword = async function ( password ) {
+    return await bcrypt.compare(password, this.password)
+}
 
 export const User = mongoose.model("User", userSchema);
