3cb98e2909f5129faadd9485491b64b00884c29f
Deepak Prasad Gupta <deepak2006gupta@outlook.com>
2026-06-02 12:07:42 +0530

    all setup configured and testing done

--- src/app.js
+++ src/app.js
@@ -22,4 +22,11 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+
+//routes import
+import userRoutes from './routes/user.routes..js';
+
+//routes declaration
+app.use("/api/v1/users", userRoutes);
+
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,25 +1,25 @@
-const asyncHandler = (fn) => async(req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-        success: false,
-        message: error.message
-    });
-  }
-};
+// const asyncHandler = (fn) => async(req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//         success: false,
+//         message: error.message
+//     });
+//   }
+// };
 
-export { asyncHandler };
+// export { asyncHandler };
 
 
 // Alternative implementation using Promise.resolve
 
-/*
+
 
 const asyncHandler = (asyncFn) =>{
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(asyncFn(req, res, next)).catch(err => next(err));
     }
 }
 
-*/
+export { asyncHandler };
