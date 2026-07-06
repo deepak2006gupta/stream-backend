3f48f27bd87e90ae23d7ca007fe8453679acbd67
Deepak Prasad Gupta <deepak2006gupta@outlook.com>
2026-07-06 21:35:51 +0530

    add reset password route to user routes and improve code formatting

--- src/routes/user.routes..js
+++ src/routes/user.routes..js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
-import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage} from '../controllers/user.controller.js';
+import { registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage,
+    resetUserPassword
+} from '../controllers/user.controller.js';
+
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -19,11 +30,12 @@ router.route("/logout").post( verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 
 router.route("/change-password").patch(verifyJWT, changeCurrentPassword);
+router.route("/reset-password").post(changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
-router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 
 export default router;
